4a7001452a44ca0bbd9d16b5d0be27b8b6e04824
fares <gabsi.fares18@gmail.com>
2023-07-26 15:28:41 +0100

    Initial commit

--- src/main/java/com/example/StageEte/RestApiwebServices/Webservices/WebServiceController.java
+++ src/main/java/com/example/StageEte/RestApiwebServices/Webservices/WebServiceController.java
@@ -1,6 +1,5 @@
 package com.example.StageEte.RestApiwebServices.Webservices;
 
-
 import java.net.URI;
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -23,30 +22,29 @@ import com.example.StageEte.RestApiwebServices.Entity.Product.Product;
 import com.example.StageEte.RestApiwebServices.Entity.Product.ProductRepository;
 import com.example.StageEte.RestApiwebServices.Exception.IdNotFoundException;
 
-import jakarta.validation.Valid; 
+import jakarta.validation.Valid;
 
 @RestController
 public class WebServiceController {
 
-private ClientRepository clientRepository;
-private CommandeRepository commandeRepository;
-private ProductRepository productRepository;
+    private ClientRepository clientRepository;
+    private CommandeRepository commandeRepository;
+    private ProductRepository productRepository;
 
-
-
-    public WebServiceController(ClientRepository clientRepository, CommandeRepository commandeRepository, ProductRepository productRepository) {
+    public WebServiceController(ClientRepository clientRepository, CommandeRepository commandeRepository,
+            ProductRepository productRepository) {
         this.clientRepository = clientRepository;
         this.commandeRepository = commandeRepository;
         this.productRepository = productRepository;
     }
-    
+
     // Permet de lister tous les clients
     @GetMapping("/clients")
-    public List<Client> retrieveAllClient(){
+    public List<Client> retrieveAllClient() {
         return clientRepository.findAll();
     }
 
-// Permet de créer un client depuis un formulaire (Webservice 1 et 2)
+    // Permet de créer un client depuis un formulaire (Webservice 1 et 2)
     @PostMapping("/clients")
     public ResponseEntity<Client> createUser(@Valid @RequestBody Client client) {
         Client savedClient = clientRepository.save(client);
@@ -59,44 +57,45 @@ private ProductRepository productRepository;
 
     // Permet de récupérer un client par son identifiant
     @GetMapping("/clients/{id}")
-    public Client retrieveClient(@PathVariable int id){
-       return clientRepository.findById(id)
-        .orElseThrow(() -> new IdNotFoundException(id));  
+    public Client retrieveClient(@PathVariable int id) {
+        return clientRepository.findById(id)
+                .orElseThrow(() -> new IdNotFoundException(id));
     }
-        
+
     // Permet de récupérer les commandes d'un client par son identifiant
     @GetMapping("/clients/{id}/commandes")
-    private List<Commande> retrieveCommandesForClient(@PathVariable int id){
-        List<Commande> commandes=clientRepository.findById(id).get().getListCommande();
+    private List<Commande> retrieveCommandesForClient(@PathVariable int id) {
+        List<Commande> commandes = clientRepository.findById(id).get().getListCommande();
         return commandes;
     }
 
     // Permet de créer une commande pour un client donné
     @PostMapping("clients/{id}/commandes")
-    public ResponseEntity<Commande> createCommande(@PathVariable int id,@Valid @RequestBody CommandeRequest commande) {
-        Client client=clientRepository.getReferenceById(id);
+    public ResponseEntity<Commande> createCommande(@PathVariable int id, @Valid @RequestBody CommandeRequest commande) {
+        Client client = clientRepository.getReferenceById(id);
         if (client == null) {
             throw new IdNotFoundException(id);
         }
         List<Integer> productIds = commande.productIds();
 
-    // Valider les identifiants des produits et récupérer les produits correspondants
-    List<Product> products = new ArrayList<>();
-    double prixTotal=0;
-    for (Integer productId : productIds) {
-        Product product = productRepository.getReferenceById(productId);
-        if (product == null) {
-            return ResponseEntity.badRequest().build();
+        // Valider les identifiants des produits et récupérer les produits
+        // correspondants
+        List<Product> products = new ArrayList<>();
+        double prixTotal = 0;
+        for (Integer productId : productIds) {
+            Product product = productRepository.getReferenceById(productId);
+            if (product == null) {
+                return ResponseEntity.badRequest().build();
+            }
+            products.add(product);
+            prixTotal += product.getPrice();
         }
-        products.add(product);
-        prixTotal+=product.getPrice();
-    }
-    Commande cmd=new Commande();
-    cmd.setClient(client);
-    cmd.setListProduit(products);
-    cmd.setCommandeDate(LocalDate.now());
-    cmd.setPrixTotal(prixTotal);
-    Commande savedCommande=commandeRepository.save(cmd);
+        Commande cmd = new Commande();
+        cmd.setClient(client);
+        cmd.setListProduit(products);
+        cmd.setCommandeDate(LocalDate.now());
+        cmd.setPrixTotal(prixTotal);
+        Commande savedCommande = commandeRepository.save(cmd);
         URI location = ServletUriComponentsBuilder.fromCurrentRequest()
                 .path("/{id}")
                 .buildAndExpand(savedCommande.getId())
@@ -104,17 +103,18 @@ private ProductRepository productRepository;
         return ResponseEntity.created(location).build();
     }
 
-    // Permet de récupérer une commande spécifique d'un client par son identifiant et l'identifiant de la commande
+    // Permet de récupérer une commande spécifique d'un client par son identifiant
+    // et l'identifiant de la commande
     @GetMapping("/clients/{id}/commandes/{id_cmd}")
-    private Commande retrieveCommandesForClient(@PathVariable int id,@PathVariable int id_cmd){
-       List<Commande> listcommandes=clientRepository.findById(id).get().getListCommande();
-       Commande commandes = listcommandes.get(id_cmd);
+    private Commande retrieveCommandesForClient(@PathVariable int id, @PathVariable int id_cmd) {
+        List<Commande> listcommandes = clientRepository.findById(id).get().getListCommande();
+        Commande commandes = listcommandes.get(id_cmd - 1);
         return commandes;
     }
 
     // Permet de lister tous les produits
     @GetMapping("/produits")
-    public List<Product> retrieveAllProducts(){
+    public List<Product> retrieveAllProducts() {
         return productRepository.findAll();
     }
 
@@ -131,18 +131,15 @@ private ProductRepository productRepository;
 
     // Permet de récupérer un produit par son identifiant
     @GetMapping("/produits/{id}")
-    public Optional<Product> retrieveProduct(@PathVariable int id){
-        Optional<Product> produit=productRepository.findById(id);
+    public Optional<Product> retrieveProduct(@PathVariable int id) {
+        Optional<Product> produit = productRepository.findById(id);
         return produit;
     }
 
+    /**
+     * Ligne_Commande
+     */
+    public record CommandeRequest(List<Integer> productIds) {
+    }
 
-
-
-/**
- * Ligne_Commande
- */
-public record CommandeRequest(List<Integer> productIds) {}
-    
 }
-
